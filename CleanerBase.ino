77a96087929d596e3499018d5c0c3019a5382228
Your Name <you@example.com>
2023-12-14 14:52:46 +0800

    Changed files to fit clbrobot

--- CleanerBase.ino
+++ CleanerBase.ino
@@ -1,3 +1,82 @@
+/*********************************************************************
+ *  ROSArduinoBridge
+ 
+    A set of simple serial commands to control a differential drive
+    robot and receive back sensor and odometry data. Default 
+    configuration assumes use of an Arduino Mega + Pololu motor
+    controller shield + Robogaia Mega Encoder shield.  Edit the
+    readEncoder() and setMotorSpeed() wrapper functions if using 
+    different motor controller or encoder method.
+
+    Created for the Pi Robot Project: http://www.pirobot.org
+    and the Home Brew Robotics Club (HBRC): http://hbrobotics.org
+    
+    Authors: Patrick Goebel, James Nugen
+
+    Inspired and modeled after the ArbotiX driver by Michael Ferguson
+    
+    Software License Agreement (BSD License)
+
+    Copyright (c) 2012, Patrick Goebel.
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
+
+     * Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+     * Redistributions in binary form must reproduce the above
+       copyright notice, this list of conditions and the following
+       disclaimer in the documentation and/or other materials provided
+       with the distribution.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+    FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+    COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+    BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+    LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+    ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+#define USE_BASE      // Enable the base controller code
+// #undef USE_BASE     // Disable the base controller code
+
+/* Define the motor controller and encoder library you are using */
+#ifdef USE_BASE
+   /* The Pololu VNH5019 dual motor driver shield */
+//    #define POLOLU_VNH5019
+
+   /* The Pololu MC33926 dual motor driver shield */
+   //#define POLOLU_MC33926
+
+   /* The RoboGaia encoder shield */
+//    #define ROBOGAIA
+   
+   /* Encoders directly attached to Arduino board */
+   //#define ARDUINO_ENC_COUNTER
+   #define ARDUINO_MY_COUNTER
+
+   /* L298 Motor driver*/
+   //#define L298_MOTOR_DRIVER
+   #define L298P_MOTOR_DRIVER
+#endif
+
+// #define USE_SERVOS  // Enable use of PWM servos as defined in servos.h
+#undef USE_SERVOS     // Disable use of PWM servos
+
+/* Serial port baud rate */
+#define BAUDRATE     57600
+
+/* Maximum PWM signal */
+#define MAX_PWM        255
+
 #if defined(ARDUINO) && ARDUINO >= 100
 #include "Arduino.h"
 #else
@@ -7,30 +86,39 @@
 /* Include definition of serial commands */
 #include "commands.h"
 
-// #include <ModbusMaster.h>
-// #include <SoftwareSerial.h>
-/* Motor driver function definitions */
-#include "servo_motor_driver.h"
+/* Sensor functions */
+#include "sensors.h"
 
-/* Cross Coupling Control parameters and functions */
-#include "CCC.h"
+/* Include servo support if required */
+#ifdef USE_SERVOS
+   #include <Servo.h>
+   #include "servos.h"
+#endif
 
-/* Run the CCC loop at 30 times per second */
-#define CCC_RATE           50     // Hz
+#ifdef USE_BASE
+  /* Motor driver function definitions */
+  #include "motor_driver.h"
 
-/* Convert the rate into an interval */
-const int CCC_INTERVAL = 1000 / CCC_RATE;
+  /* Encoder driver function definitions */
+  #include "encoder_driver.h"
 
-/* Track the next time we make a PID calculation */
-unsigned long nextCCC = CCC_INTERVAL;
+  /* PID parameters and functions */
+  #include "diff_controller.h"
 
-/* Stop the robot if it hasn't received a movement command
-in this number of milliseconds */
-//  电机接收到速度指令后的运行时间
-#define AUTO_STOP_INTERVAL 5000
-long lastMotorCommand = AUTO_STOP_INTERVAL;
+  /* Run the PID loop at 30 times per second */
+  #define PID_RATE           30     // Hz
 
+  /* Convert the rate into an interval */
+  const int PID_INTERVAL = 1000 / PID_RATE;
+  
+  /* Track the next time we make a PID calculation */
+  unsigned long nextPID = PID_INTERVAL;
 
+  /* Stop the robot if it hasn't received a movement command
+   in this number of milliseconds */
+  #define AUTO_STOP_INTERVAL 2000
+  long lastMotorCommand = AUTO_STOP_INTERVAL;
+#endif
 
 /* Variable initialization */
 
@@ -51,7 +139,7 @@ char argv2[16];
 // The arguments converted to integers
 long arg1;
 long arg2;
-int i;
+
 /* Clear the current command parameters */
 void resetCommand() {
   cmd = NULL;
@@ -65,243 +153,202 @@ void resetCommand() {
 
 /* Run a command.  Commands are defined in commands.h */
 int runCommand() {
-    int i = 0;
-    char *p = argv1;
-    char *str;
-    int CCC_args[8];
-    arg1 = atoi(argv1);
-    arg2 = atoi(argv2);
+  int i = 0;
+  char *p = argv1;
+  char *str;
+  int pid_args[4];
+  arg1 = atoi(argv1);
+  arg2 = atoi(argv2);
+  
+  switch(cmd) {
+  case GET_BAUDRATE:
+    Serial.println(BAUDRATE);
+    break;
+  case ANALOG_READ:
+    Serial.println(analogRead(arg1));
+    break;
+  case DIGITAL_READ:
+    Serial.println(digitalRead(arg1));
+    break;
+  case ANALOG_WRITE:
+    analogWrite(arg1, arg2);
+    Serial.println("OK"); 
+    break;
+  case DIGITAL_WRITE:
+    if (arg2 == 0) digitalWrite(arg1, LOW);
+    else if (arg2 == 1) digitalWrite(arg1, HIGH);
+    Serial.println("OK"); 
+    break;
+  case PIN_MODE:
+    if (arg2 == 0) pinMode(arg1, INPUT);
+    else if (arg2 == 1) pinMode(arg1, OUTPUT);
+    Serial.println("OK");
+    break;
+  case PING:
+    Serial.println(Ping(arg1));
+    break;
+#ifdef USE_SERVOS
+  case SERVO_WRITE:
+    servos[arg1].setTargetPosition(arg2);
+    Serial.println("OK");
+    break;
+  case SERVO_READ:
+    Serial.println(servos[arg1].getServo().read());
+    break;
+#endif
     
-    switch(cmd) {
-    case GET_BAUDRATE:
-        Serial.println(BAUDRATE);
-        break;
-    case ANALOG_READ:
-        Serial.println(analogRead(arg1));
-        break;
-    case DIGITAL_READ:
-        Serial.println(digitalRead(arg1));
-        break;
-    case ANALOG_WRITE:
-        analogWrite(arg1, arg2);
-        Serial.println("OK"); 
-        break;
-    case DIGITAL_WRITE:
-        if (arg2 == 0) digitalWrite(arg1, LOW);
-        else if (arg2 == 1) digitalWrite(arg1, HIGH);
-        Serial.println("OK"); 
-        break;
-    case PIN_MODE:
-        if (arg2 == 0) pinMode(arg1, INPUT);
-        else if (arg2 == 1) pinMode(arg1, OUTPUT);
-        Serial.println("OK");
-        break;
-    case PING:
-        Serial.println(Ping(arg1));
-        break;
-    case READ_ENCODERS:
-        Serial.print(readEncoder(LEFT));
-        Serial.print(" ");
-        Serial.println(readEncoder(RIGHT));
-        break;
-    case READ_VEL:
-        Serial.print(readVel(LEFT));
-        Serial.print(" ");
-        Serial.println(readVel(RIGHT));
-        break;
-    case RESET_ENCODERS:
-        resetEncoders();
-        resetCrossCoupl();
-        Serial.println("OK");
-        break;
-    case MOTOR_SPEEDS:
-        /* Reset the auto stop timer */
-        lastMotorCommand = millis();
-        if (arg1 == 0 && arg2 == 0) {
-            setMotorSpeeds(0, 0);
-            resetCrossCoupl();
-            moving = 0;
-        }
-        else moving = 1;
-        // 设置CCC调试的目标
-        sLeftCrossCoupl.targetVel = arg1;
-        sRightCrossCoupl.targetVel = arg2;
-
-        Serial.println("OK"); 
-        break;
-    case UPDATE_CCC:
-        while ((str = strtok_r(p, ":", &p)) != '\0') {
-            CCC_args[i] = atoi(str);
-            i++;
-        }
-        sLeftCrossCoupl.Kp = CCC_args[0];
-        sLeftCrossCoupl.Kd = CCC_args[1];
-        sLeftCrossCoupl.Ki = CCC_args[2];
-
-        sRightCrossCoupl.Kp = CCC_args[3];
-        sRightCrossCoupl.Kd = CCC_args[4];
-        sRightCrossCoupl.Ki = CCC_args[5];
-
-        Serial.println("OK");
-        break;
-    default:
-        Serial.println("Invalid Command");
-        break;
+#ifdef USE_BASE
+  case READ_ENCODERS:
+    Serial.print(readEncoder(LEFT));
+    Serial.print(" ");
+    Serial.println(readEncoder(RIGHT));
+    break;
+   case RESET_ENCODERS:
+    resetEncoders();
+    resetPID();
+    Serial.println("OK");
+    break;
+  case MOTOR_SPEEDS:
+    /* Reset the auto stop timer */
+    lastMotorCommand = millis();
+    if (arg1 == 0 && arg2 == 0) {
+      setMotorSpeeds(0, 0);
+      resetPID();
+      moving = 0;
     }
+    else moving = 1;
+    leftPID.TargetTicksPerFrame = arg1;
+    rightPID.TargetTicksPerFrame = arg2;
+    Serial.println("OK"); 
+    break;
+  case UPDATE_PID:
+    while ((str = strtok_r(p, ":", &p)) != '\0') {
+       pid_args[i] = atoi(str);
+       i++;
+    }
+    Kp = pid_args[0];
+    Kd = pid_args[1];
+    Ki = pid_args[2];
+    Ko = pid_args[3];
+    Serial.println("OK");
+    break;
+#endif
+  default:
+    Serial.println("Invalid Command");
+    break;
+  }
 }
 
+/* Setup function--runs once at startup. */
 void setup() {
+  Serial.begin(BAUDRATE);
+
+// Initialize the motor controller if used */
+#ifdef USE_BASE
+  #ifdef ARDUINO_ENC_COUNTER
+    //set as inputs
+    DDRD &= ~(1<<LEFT_ENC_PIN_A);
+    DDRD &= ~(1<<LEFT_ENC_PIN_B);
+    DDRC &= ~(1<<RIGHT_ENC_PIN_A);
+    DDRC &= ~(1<<RIGHT_ENC_PIN_B);
     
-    initMotorController();
-    resetCrossCoupl();
+    //enable pull up resistors
+    PORTD |= (1<<LEFT_ENC_PIN_A);
+    PORTD |= (1<<LEFT_ENC_PIN_B);
+    PORTC |= (1<<RIGHT_ENC_PIN_A);
+    PORTC |= (1<<RIGHT_ENC_PIN_B);
     
-}
-//  /*
-void loop() {
-
-    // Serial.println(Kp);
-    while (Serial.available() > 0) {
+    // tell pin change mask to listen to left encoder pins
+    PCMSK2 |= (1 << LEFT_ENC_PIN_A)|(1 << LEFT_ENC_PIN_B);
+    // tell pin change mask to listen to right encoder pins
+    PCMSK1 |= (1 << RIGHT_ENC_PIN_A)|(1 << RIGHT_ENC_PIN_B);
     
-        // Read the next character
-        chr = Serial.read();
-
-        // Terminate a command with a CR
-        if (chr == 13) {
-            if (arg == 1) argv1[index] = NULL;
-            else if (arg == 2) argv2[index] = NULL;
-            runCommand();
-            resetCommand();
-            // Serial.println("execute in "chr == 13" ");
-        }
-        // Use spaces to delimit parts of the command
-        else if (chr == ' ') {
-            // Step through the arguments
-            if (arg == 0) arg = 1;
-            else if (arg == 1)  {
-                argv1[index] = NULL;
-                arg = 2;
-                index = 0;
-            }
-            // Serial.println("execute in "chr == ' '" ");
-            continue;
-        }
-        else {
-            // Serial.println("execute in "chr == '  2'" ");
-            if (arg == 0) {
-                // The first arg is the single-letter command
-                cmd = chr;
-            }
-            else if (arg == 1) {
-                // Subsequent arguments can be more than one character
-                argv1[index] = chr;
-                index++;
-            }
-            else if (arg == 2) {
-                argv2[index] = chr;
-                index++;
-            }
-        }
-    }
-
-
-    if (millis() > nextCCC) {
-        updateCrossCoupl();
-
-        nextCCC += CCC_INTERVAL;
-        // Serial.println("execute in millis() > nextCCC ");
-    }
+    // enable PCINT1 and PCINT2 interrupt in the general interrupt mask
+    PCICR |= (1 << PCIE1) | (1 << PCIE2);
+  #elif defined ARDUINO_MY_COUNTER
+  initEncoders();
+  #endif
   
-    // Check to see if we have exceeded the auto-stop interval
-    if ((millis() - lastMotorCommand) > AUTO_STOP_INTERVAL) {;
-        setMotorSpeeds(0, 0);
-        moving = 0;
-        // Serial.println("execute in (millis() - lastMotorCommand) > AUTO_STOP_INTERVAL ");
+  initMotorController();
+  resetPID();
+#endif
+
+/* Attach servos if used */
+  #ifdef USE_SERVOS
+    int i;
+    for (i = 0; i < N_SERVOS; i++) {
+      servos[i].initServo(
+          servoPins[i],
+          stepDelay[i],
+          servoInitPosition[i]);
     }
+  #endif
 }
-//  */
 
-/*
-void loop()
-{
-    while (Serial.available() > 0) {
-    
-        // Read the next character
-        chr = Serial.read();
-
-        // Terminate a command with a CR
-        if (chr == 13) {
-            if (arg == 1) argv1[index] = NULL;
-            else if (arg == 2) argv2[index] = NULL;
-            runCommand();
-            resetCommand();
-            // Serial.println("execute in "chr == 13" ");
-        }
-        // Use spaces to delimit parts of the command
-        else if (chr == ' ') {
-            // Step through the arguments
-            if (arg == 0) arg = 1;
-            else if (arg == 1)  {
-                argv1[index] = NULL;
-                arg = 2;
-                index = 0;
-            }
-            // Serial.println("execute in "chr == ' '" ");
-            continue;
-        }
-        else {
-            // Serial.println("execute in "chr == '  2'" ");
-            if (arg == 0) {
-                // The first arg is the single-letter command
-                cmd = chr;
-            }
-            else if (arg == 1) {
-                // Subsequent arguments can be more than one character
-                argv1[index] = chr;
-                index++;
-            }
-            else if (arg == 2) {
-                argv2[index] = chr;
-                index++;
-            }
-        }
-    }
-
-    // uint32_t u32StartTime = millis();
-    // updateCrossCoupl();
-    setMotorSpeeds(200, 200);
-    // Serial.println(millis() - u32StartTime);
-    // Check to see if we have exceeded the auto-stop interval
-    if ((millis() - lastMotorCommand) > AUTO_STOP_INTERVAL) {;
-        setMotorSpeeds(0, 0);
-        moving = 0;
-        // Serial.println("execute in (millis() - lastMotorCommand) > AUTO_STOP_INTERVAL ");
-    }
-}
+/* Enter the main loop.  Read and parse input from the serial port
+   and run any valid commands. Run a PID calculation at the target
+   interval and check for auto-stop conditions.
 */
-
-/*
 void loop() {
+  while (Serial.available() > 0) {
+    
+    // Read the next character
+    chr = Serial.read();
+
+    // Terminate a command with a CR
+    if (chr == 13) {
+      if (arg == 1) argv1[index] = NULL;
+      else if (arg == 2) argv2[index] = NULL;
+      runCommand();
+      resetCommand();
+    }
+    // Use spaces to delimit parts of the command
+    else if (chr == ' ') {
+      // Step through the arguments
+      if (arg == 0) arg = 1;
+      else if (arg == 1)  {
+        argv1[index] = NULL;
+        arg = 2;
+        index = 0;
+      }
+      continue;
+    }
+    else {
+      if (arg == 0) {
+        // The first arg is the single-letter command
+        cmd = chr;
+      }
+      else if (arg == 1) {
+        // Subsequent arguments can be more than one character
+        argv1[index] = chr;
+        index++;
+      }
+      else if (arg == 2) {
+        argv2[index] = chr;
+        index++;
+      }
+    }
+  }
   
-  updateCrossCoupl();
-  delay(3000);
-//  sLeftCrossCoupl.targetVel = 200;
-//  sRightCrossCoupl.targetVel = 200;
-//  updateCrossCoupl();
-//  delay(1000);
-//  sLeftCrossCoupl.targetVel = 400;
-//  sRightCrossCoupl.targetVel = 200;
-//  updateCrossCoupl();
-//  delay(3000);
-//  sLeftCrossCoupl.targetVel = 0;
-//  sRightCrossCoupl.targetVel = 0;
-//  updateCrossCoupl();
-//  delay(3000);
-//  setMotorSpeeds(0, 0);for(;;){};
-//  uint32_t startTime = millis();
-//  while(1){
-//    int vel = readFilterVel(RIGHT);
-//    Serial.println(vel);
-//    if(millis() - startTime > 1500) {setMotorSpeeds(0, 0);for(;;){};}
-//  } 
+// If we are using base control, run a PID calculation at the appropriate intervals
+#ifdef USE_BASE
+  if (millis() > nextPID) {
+    updatePID();
+    nextPID += PID_INTERVAL;
+  }
+  
+  // Check to see if we have exceeded the auto-stop interval
+  if ((millis() - lastMotorCommand) > AUTO_STOP_INTERVAL) {;
+    setMotorSpeeds(0, 0);
+    moving = 0;
+  }
+#endif
+
+// Sweep servos
+#ifdef USE_SERVOS
+  int i;
+  for (i = 0; i < N_SERVOS; i++) {
+    servos[i].doSweep();
+  }
+#endif
 }
-*/
